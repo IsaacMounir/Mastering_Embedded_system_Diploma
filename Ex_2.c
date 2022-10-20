2c9ff7fdd453f504d2be1f87983fc2205c55b0e8
IsaacMounir <ashacmoner@gmail.com>
2022-10-21 00:42:53 +0200

    C_Program_To_Print_a_Integer_Entered_by_a_User

---
+++ Ex_2.c
@@ -0,0 +1,19 @@
+
+/*
+ ======================================================================================================================
+ Name        : EX2_ C Program to Print a Integer Entered by a User
+ Author      : Isaac Mounir 
+ Created on	 : oct 20, 2022
+ Description : Unit 2 C-Programming >> Assignment 2 C-Basic >> EX2_ C Program to Print a Integer Entered by a User using Ansi-style (c99)
+ ==============================================================================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the Number =  ");
+    scanf("%d",&x);
+    printf("your Number is %d \n ",x);
+    return 0;
+}
