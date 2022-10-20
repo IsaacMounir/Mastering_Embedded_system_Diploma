c46d8a768a173abc2da0a9603c50e95b901e04fe
IsaacMounir <ashacmoner@gmail.com>
2022-10-21 01:25:24 +0200

    C_Program_to_Add_Two_Integers

---
+++ Unit_2_C_Programming/C_Basics_Assignments/Ex_3.c
@@ -0,0 +1,22 @@
+
+/*
+ ======================================================================================================================
+ Name        : EX3_ C Program to Add Two Integers
+ Author      : Isaac Mounir 
+ Created on	 : oct 20, 2022
+ Description : Unit 2 C-Programming >> Assignment 1 C-Basic >> EX3_ C Program to Add Two Integers using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    int x , y ;
+    int sum ;
+    printf("Enter the first Number =    ");
+    scanf("%d",&x);
+    printf("enter the second Number =   ");
+    scanf("%d",&y);
+    printf("the sum =  %d \n ",x+y);
+    return 0;
+}
