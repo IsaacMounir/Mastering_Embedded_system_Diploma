77e0fb71271f424711da15269c497273042cf51f
IsaacMounir <ashacmoner@gmail.com>
2022-10-24 21:16:54 +0200

    C Program to check a number is even or odd

--- Unit_2_C_Programming/Loob&Condition_Assignments/Ex_1.c
+++ Unit_2_C_Programming/Loob&Condition_Assignments/Ex_1.c
@@ -1,6 +1,6 @@
 /*
  ======================================================================================================================
- Name        : EX1_C_Program_to_Check_a_Number_Is_Even.c
+ Name        : EX1_C_Program_to_Check_a_Number_Is_Even
  Author      : Isaac Mounir
  Created on	 : oct 24, 2022
  Description : Unit 2 C-Programming >> Assignment 2 C-Basic >> EX1 C Program to check a number is even using Ansi-style (c99)
@@ -13,7 +13,7 @@ int main()
     int number;
     printf("Enter the number to check even or odd  = ");
     scanf("%d", &number);
-    if ((number % 2) == 0)
+    if ((number % 2) == 0)  // check the remainder is 0 or not //
     {
         printf(" %d is even ", number);
     }
