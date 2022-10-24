3e18ea4e58401fc39705df50caba7e31e8afce41
IsaacMounir <ashacmoner@gmail.com>
2022-10-24 21:17:45 +0200

    C Program to Check Vowel or Consonant Letter

---
+++ Unit_2_C_Programming/Loob&Condition_Assignments/Ex_2.c
@@ -0,0 +1,23 @@
+/*
+ ======================================================================================================================
+ Name        : EX1_C_Program_to_Check_Vowel_or_Consonant_Letter
+ Author      : Isaac Mounir
+ Created on	 : oct 24, 2022
+ Description : Unit 2 C-Programming >> Assignment 2 C-Basic >> EX1 C Program to Check Vowel or Consonant_Letter using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    char x;
+    printf("Enter the char to check vowels or consonants   ");
+    scanf("%c", &x);
+    if (x == 'a' || x == 'e' || x == 'o' || x == 'u' || x == 'i' || x == 'A' || x == 'E' || x == 'O' || x == 'U' || x == 'I')
+    {
+        printf(" %c is vowels ", x);
+    }
+    else
+        printf(" %c is consonants", x);
+    return 0;
+}
