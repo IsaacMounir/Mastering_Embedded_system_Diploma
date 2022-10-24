edc4906c98fa24d01843ea1f113dab3818e6053f
IsaacMounir <ashacmoner@gmail.com>
2022-10-24 20:08:27 +0200

    Program to Source Code to Swap Two Numbers

---
+++ Unit_2_C_Programming/C_Basics_Assignments/Ex_6.c
@@ -0,0 +1,27 @@
+/*
+ ======================================================================================================================
+ Name        : EX6_C Program to Source Code to Swap Two Numbers
+ Author      : Isaac Mounir
+ Created on	 : oct 24, 2022
+ Description : Unit 2 C-Programming >> Assignment 1 C-Basic >> EX6_ C Program to Source Code to Swap Two Numbers using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int temp;
+    printf("Enter the value of a = ");
+    scanf("%d", &a);
+    printf("Enter the value of b = ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("after of swap ,the value of a = %d\n", a);
+    printf("after of swap ,the value of b = %d", b);
+
+    return 0;
+}
